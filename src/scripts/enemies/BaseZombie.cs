08eb1ddf29aabdd2ffc891e08e69620dc7df3f20
sebastian.zumbrunn <sebastian.zumbrunn@pm.me>
2023-02-03 21:44:10 +0100

    apply formating

--- src/scripts/enemies/BaseZombie.cs
+++ src/scripts/enemies/BaseZombie.cs
@@ -3,56 +3,56 @@ using System;
 
 public class BaseZombie : KinematicBody2D
 {
-    [Export]
-    private int MaxHealth = 10;
+	[Export]
+	private int MaxHealth = 10;
 
-    [Export]
-    private int Health = 1;
+	[Export]
+	private int Health = 1;
 
-    [Export]
-    private float MaxSpeed = 1.0F;
+	[Export]
+	private float MaxSpeed = 1.0F;
 
-    [Export]
-    private float Speed;
+	[Export]
+	private float Speed;
 
-    [Export]
-    private NodePath Target;
+	[Export]
+	private NodePath Target;
 
-    private NavigationAgent2D agent;
+	private NavigationAgent2D agent;
 
-    public override void _Ready()
-    {
-        base._Ready();
+	public override void _Ready()
+	{
+		base._Ready();
 
-        // set the speed to the max speed
-        Speed = MaxSpeed;
+		// set the speed to the max speed
+		Speed = MaxSpeed;
 
-        agent = GetNode<NavigationAgent2D>("NavigationAgent2D");
-        agent.SetTargetLocation(GetNode<Node2D>(Target).GlobalPosition);
-    }
+		agent = GetNode<NavigationAgent2D>("NavigationAgent2D");
+		agent.SetTargetLocation(GetNode<Node2D>(Target).GlobalPosition);
+	}
 
-    public override void _PhysicsProcess(float delta)
-    {
-        base._PhysicsProcess(delta);
+	public override void _PhysicsProcess(float delta)
+	{
+		base._PhysicsProcess(delta);
 
-        if (agent.IsTargetReached()) return;
+		if (agent.IsTargetReached()) return;
 
-        Vector2 currentPosition = GlobalTransform.origin;
-        Vector2 nextPathPosition = agent.GetNextLocation();
+		Vector2 currentPosition = GlobalTransform.origin;
+		Vector2 nextPathPosition = agent.GetNextLocation();
 
-        Vector2 nextVelocity = (nextPathPosition - currentPosition).Normalized() * Speed;
+		Vector2 nextVelocity = (nextPathPosition - currentPosition).Normalized() * Speed;
 
-        MoveAndSlide(nextVelocity);
-    }
+		MoveAndSlide(nextVelocity);
+	}
 
 
-    public void OnHit()
-    {
-        Health += 1;
-        if (Health >= MaxHealth)
-        {
-            QueueFree();
-        }
-    }
+	public void OnHit()
+	{
+		Health += 1;
+		if (Health >= MaxHealth)
+		{
+			QueueFree();
+		}
+	}
 
 }
