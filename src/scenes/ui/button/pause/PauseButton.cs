0a18f1b865fd83d30d36bddd4d730b37652d0de0
Jonas Costa <contact@jonascosta.ch>
2023-02-04 18:24:20 +0100

    Add pause button

--- src/scenes/ui/button/pause/PauseButton.cs
+++ src/scenes/ui/button/pause/PauseButton.cs
@@ -1,21 +1,16 @@
 using Godot;
 using System;
 
-public class PauseButton : Control
+public class PauseButton : AnimatedButton
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
 
-    // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+        base._Ready();
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    public override void onButtonPressed()
+    {
+        GetTree().Paused = !GetTree().Paused;
+    }
 }
