2a160ef93cf96a5ef7117a651f7cca068d36c89d
balkenic <balkenic@students.zhaw.ch>
2023-02-05 19:22:19 +0100

    Bat fixed and palyer HP fixed

--- src/scenes/enemies/bat/Bat.cs
+++ src/scenes/enemies/bat/Bat.cs
@@ -4,15 +4,22 @@ using System;
 public class Bat : BaseEnemy
 {
 
+    Vector2 currentPosition{get; set;}
+    private Vector2 targetVector = new Vector2();
+
     public override void _Ready()
     {
-        //LookAt(GetNode<Node2D>(Target).Position);
+        currentPosition = GlobalPosition;
+        targetVector = TargetNode.Position - currentPosition;
     }
 
     public override void _PhysicsProcess(float delta)
     {
         base._PhysicsProcess(delta);
 
-        MoveAndSlide(new Vector2(1, 0) * Speed);
+        //MoveAndSlide(new Vector2(1, 0) * Speed);
+
+        Vector2 nextVelocity = targetVector.Normalized() * Speed * delta;
+        GlobalPosition += nextVelocity;
     }
 }
