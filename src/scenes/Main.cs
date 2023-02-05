e597117dbea932b23e600561e436b9dd9da698b8
Jonas Costa <contact@jonascosta.ch>
2023-02-05 11:35:25 +0100

    Add pedestal

--- src/scenes/Main.cs
+++ src/scenes/Main.cs
@@ -1,16 +1,34 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public class Main : Node2D
 {
+    private List<Pedestal> pedestals;
+
+    [Signal]
+    delegate void towerSelected(PackedScene towerType);
+    [Signal]
+    delegate void towerDeselected();
 
     public override void _Ready()
     {
         Input.MouseMode = Input.MouseModeEnum.Hidden;
+
+        foreach (Node node in GetChildren())
+        {
+            if (node is Pedestal pedestal) {
+                Connect(nameof(towerSelected), pedestal, "onTowerSelected");
+                Connect(nameof(towerDeselected), pedestal, "onTowerDeselected");
+            }
+        }
     }
 
-    public override void _Process(float delta)
-    {
-        
+    private void onTowerSelected(PackedScene towerType) {
+        EmitSignal(nameof(towerSelected), towerType);
+    }
+
+    private void onTowerDeselected() {
+        EmitSignal(nameof(towerDeselected));
     }
 }
---
+++ src/scenes/tiles/Pedestal/Pedestal.cs
@@ -0,0 +1,70 @@
+using Godot;
+using System;
+
+public class Pedestal : Node2D
+{
+    [Export]
+    private float towerOpacity = 0.5f;
+
+    [Export]
+    private NodePath towerAnchorPath;
+    private Node2D towerAnchor;
+
+    [Export]
+    private NodePath availabilityPath;
+    private Selection availability;
+
+    private BaseTower tower;
+
+    private bool reserved = false;
+
+    public override void _Ready()
+    {
+        availability = GetNode<Selection>(availabilityPath);
+        towerAnchor = GetNode<Node2D>(towerAnchorPath);
+        availability.Hide();
+    }
+
+    private void onTowerSelected(PackedScene towerType) {
+        if (!reserved) {
+            availability.Show();
+            if (IsInstanceValid(tower)) {
+                tower.QueueFree();
+            }
+            tower = towerType.Instance<BaseTower>();
+            Color mod = tower.Modulate;
+            mod.a = towerOpacity;
+            tower.Modulate = mod;
+            tower.Hide();
+            towerAnchor.AddChild(tower);
+            tower.Position = new Vector2(0,0);
+        }
+    }
+
+    private void onTowerDeselected() {
+        availability.Hide();
+    }
+
+    private void onHover() {
+        if (IsInstanceValid(tower)) {
+            tower.Show();
+        }
+    }
+
+    private void onUnhover() {
+        if (IsInstanceValid(tower) && !reserved) {
+            tower.Hide();
+        }
+    }
+
+    private void onTowerSet() {
+        if (!reserved && IsInstanceValid(tower)) {
+            Color mod = tower.Modulate;
+            mod.a = 1.0f;
+            tower.Modulate = mod;
+            tower.Show();
+            reserved = true;
+            availability.toggleAvailability();
+        }
+    }
+}
